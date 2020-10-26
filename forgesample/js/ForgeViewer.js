0e2a38439b76f8d1625b60dfbb319acd8ad73148
Nedeljko Šovljanski <sovljaned@gmail.com>
2020-10-26 14:02:39 +0100

    Removing direct drawing in viewer

--- forgesample/js/ForgeViewer.js
+++ forgesample/js/ForgeViewer.js
@@ -15,46 +15,8 @@ function launchViewer(urn) {
         viewer.start();
         viewer.setLightPreset(18);
         var documentId = 'urn:' + urn;
-
-        // ovo sam ubacio iz drugog primera
-        viewer.loadExtension("Autodesk.Viewing.SceneBuilder").then(() => {
-            sceneBuilder = viewer.getExtension("Autodesk.Viewing.SceneBuilder");
-
-            sceneBuilder.addNewModel({})
-                .then((modelBuilder) => {
-                    let geom = new THREE.BufferGeometry().fromGeometry(
-                        new THREE.BoxGeometry(60, 40, 1));
-                    let phongMaterial = new THREE.MeshPhongMaterial({
-                        color: new THREE.Color(1, 0, 0)
-                    });
-                    mesh = new THREE.Mesh(geom, phongMaterial);
-                    modelBuilder.addMesh(mesh);
-
-                    const axis = new THREE.Vector3(0, 1, 0);
-                    const posVec = new THREE.Vector3(0, 0, -100);
-                    const someVec = new THREE.Vector3(1, 1, 1);
-                    const fixedQuat = new THREE.Quaternion(0, 0, 0, 1);
-
-                    let counter = 0;
-
-                    this._timer = setInterval(function () {
-                        mesh.matrix = new THREE.Matrix4().compose(
-                            posVec,
-                            fixedQuat.setFromAxisAngle(axis, counter),
-                            someVec);
-
-                        counter += 0.01;
-                        if (counter > 6.28) {
-                            counter = 0;
-                        }
-                        modelBuilder.updateMesh(mesh);
-                    }, 100);
-                    
-                });
-        });
-        // ovde se završava
-
-        //Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+        
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
     });
 }
 
